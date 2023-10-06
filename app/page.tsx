75e786203bfdc12b9a89d0e9fc5af17ba64283c5
gincode18 <vishalkamboj9211@gmail.com>
2023-10-06 19:35:35 +0530

    Initial Profile Setup logic :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 import { ModeToggle } from "@/components/ToggleTheme";
 
-export default function Home() {
-  return <div className="">
-    <ModeToggle></ModeToggle>
-    <button className="btn bg-primary">uisdfgud</button>
-  </div>;
-}
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return (
+    <>
+      <ModeToggle></ModeToggle>
+      <InitialModal />
+    </>
+  );
+};
+
+export default SetupPage;
---
+++ lib/initial-profile.tsx
@@ -0,0 +1,35 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id
+    }
+  });
+console.log("[intial-profile]");
+
+  if (profile) {
+    return profile;
+  }
+  console.log("[intial-profile:Not Found]");
+  
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress
+    }
+  });
+
+  return newProfile;
+};
